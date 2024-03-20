7b7374aa16e0c4a38807501b4b2d4dce84c32579
Carlos Paredes <carlos@paredesrobles.com>
2024-03-20 00:38:57 -0300

    feat(Reviewer): add runner steps

--- code-reviewer/reviewer.py
+++ code-reviewer/reviewer.py
@@ -7,9 +7,26 @@ class Reviewer:
         self.gh = GithubPR(gh_api_key, pull_request_url)
         self.assistant = OpenAIAssistant(openai_api_key, assistant_id)
 
+        self.steps = [
+            self.add_step(self.add_pr_description),
+            self.add_step(self.add_commit_messages, "Review the commit messages. Provide feedback on how the PR is structured"),
+            self.add_step(self.review_files, "You are gonna receive each file diff"),
+            self.add_step(self.summary_review, "All files reviewed. Give a general overview of the quality of the PR from 0 to 10")
+        ]
+
+        self.__run_steps()
+
+    def __run_steps(self):
+        for step in self.steps:
+            step()
+
     def add_pr_description(self):
         message = f"Pull Request Description: {self.gh.pr.title}\n\n{self.gh.pr.body}"
         self.assistant.add_message(message, "user")
+
+    def add_commit_messages(self):
+        messages = "\n".join(self.gh.get_commit_messages())
+        self.assistant.add_and_retrieve_message(messages, "user")
 
     def review_files(self):
         for filename in self.gh.get_modified_files():
@@ -23,3 +40,13 @@ class Reviewer:
     def summary_review(self):
         summary = self.assistant.add_and_retrieve_message("Summary Review", "user")
         self.gh.comment_on_pr(summary)
+
+    def add_step(self, func, system_message=None):
+        def wrapper(*args, **kwargs):
+            print(f"Running step: {func.__name__}")
+            if system_message:
+                self.assistant.add_message(system_message, "user")
+
+            func(*args, **kwargs)
+
+        return wrapper
